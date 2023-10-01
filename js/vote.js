682f7dea150ae27cf898ed8913895849ba7f2f7e
Кайрат <110962052+Kairat-Nobels@users.noreply.github.com>
2023-10-01 14:14:21 +0600

    add to public repository on github

---
+++ js/vote.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('.voteForm')
+const rezalts = document.querySelector('.rezalts')
+// form.onsubmit = (e) => {
+//     e.preventDefault()
+//     e.target.style.display = 'none';
+//     rezalts.classList.toggle('rezaltsHide')
+// }
+
+const down = document.querySelector('.chatDown')
+window.onload = () =>
+{
+    console.log('niz')
+    scrollTo(down)
+}
+down.onsubmit = (e) =>
+{
+    scrollTo(down)
+}
+window.scrollTo(0, document.body.scrollHeight)
